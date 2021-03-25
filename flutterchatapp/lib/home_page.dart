b2e24206e56b77488142b8b3c7535fae96632c93
Prayogkarki <prayog.karki@outlook.com>
2021-03-25 23:40:33 +0545

    adding flutter chatapp

---
+++ flutterchatapp/lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterchatapp/chat_screen.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frenzy Chat"),
+        centerTitle: true,
+      ),
+      body: ChatScreen(),
+    );
+  }
+}
+
---
+++ flutterchatapp/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutterchatapp/home_page.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Chat App",
+  home: HomePage(),
+
+
+));
+
+
